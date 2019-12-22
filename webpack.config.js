8794501c02bcb2082dc0639944f5999b13605f93
Ksenia <filippovaki@yandex.ru>
2019-12-22 13:20:27 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require(`path`);
+
+module.exports = {
+  entry: `./src/index.tsx`,
+  output: {
+    filename: `bundle.js`,
+    path: path.join(__dirname, `public`)
+  },
+  devServer: {
+    contentBase: path.join(__dirname, `public`),
+    compress: false,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {loader: `babel-loader`},
+          {loader: 'ts-loader'}
+        ]
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+  devtool: `source-map`
+};
