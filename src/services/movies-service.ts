173616f35aa82b198852f1b3b257aaf5e295380a
Ksenia <filippovaki@yandex.ru>
2020-01-02 14:44:38 +0300

    rewrite api with axios

--- src/services/movies-service.ts
+++ src/services/movies-service.ts
@@ -1,39 +1,57 @@
+import axios, { AxiosError, AxiosInstance, AxiosResponse, Method } from 'axios';
+
+import history from '../history';
+
 export default class MoviesService {
-  private readonly apiBase = 'https://htmlacademy-react-2.appspot.com/wtw';
+  private readonly api: AxiosInstance;
+  private readonly API_TIMEOUT = 5000;
+  private readonly API_BASE = 'https://htmlacademy-react-2.appspot.com/wtw';
+
+  constructor() {
+    this.api = axios.create({
+      baseURL: this.API_BASE,
+      timeout: this.API_TIMEOUT,
+      withCredentials: true,
+    });
+
+    const onSuccess = (response: AxiosResponse) => response;
+
+    const onError = (error: AxiosError) => {
+      if (
+        error.response &&
+        (error.response.status === 401 || error.response.status === 403)
+      ) {
+        history.push(`/login`);
+      }
+      return error;
+    };
+
+    this.api.interceptors.response.use(onSuccess, onError);
+  }
 
   public async getMovies() {
-    return this.getResource('/films');
+    return this.makeRequest('/films');
   }
 
   public getReviews(movieId: number) {
-    return this.getResource(`/comments/${movieId}`);
+    return this.makeRequest(`/comments/${movieId}`);
   }
 
   public getFavouriteMovies() {
-    return this.getResource('/favorite');
+    return this.makeRequest('/favorite');
   }
 
   public setFavouriteStatus(movieId: number, status: 0 | 1) {
-    return this.postResource(`/favorite/${movieId}/${status}`);
+    return this.makeRequest(`/favorite/${movieId}/${status}`, 'post');
   }
 
-  private async getResource(url: string) {
-    const res = await fetch(`${this.apiBase}${url}`);
+  private async makeRequest(url: string, method: Method = 'get') {
+    const response = await this.api(url, { method });
 
-    if (!res.ok) {
-      throw new Error(`Could not fetch ${url}`);
+    if (response && response.status === 200) {
+      return response.data;
     }
-    return await res.json();
-  }
 
-  private async postResource(url: string) {
-    const res = await fetch(`${this.apiBase}${url}`, {
-      method: 'post',
-    });
-
-    if (!res.ok) {
-      throw new Error(`Could not fetch ${url}`);
-    }
-    return await res.json();
+    throw new Error(`Could not fetch ${url}`);
   }
 }
